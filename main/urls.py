e2f5cb37ad651aba35e5f442a7441dbdce20b9a1
kiri3914 <t.bektur.2002@gmail.com>
2023-08-30 13:19:42 +0600

    detail, add Article

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:category_id>/', views.get_article_by_category, name='get_article_by_category'),
     path('tag/<int:tag_id>/', views.get_article_by_tag, name='get_article_by_tag'),
+    path('article/<int:article_id>/', views.detail_article, name='detail_article'),
+    path('add_article/', views.add_article, name='add_article'),
+    path('add_category/', views.add_category, name='add_category'),
 ]
